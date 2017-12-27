32e2f32c2de13b1decaeed229fdb66f8ce6718a1
kcoffrin <kcoffrin@gmail.com>
2017-12-27 06:51:40 -0500

    Selenium Course Commit
    Started Course.  First Sync

---
+++ Selenium Webdriver CSharp Bootcamp/C Sharp Setup Section/FirstLesson/FirstLesson/Program.cs
@@ -0,0 +1,16 @@
+﻿//These are libraries that are called and used
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstLesson
+{
+    class Program //Has to have one public class in order for the program to run.
+    {
+        static void Main(string[] args) // For console appplications, it all starts here. In between the {}
+        {
+        }
+    }
+}
---
+++ Selenium Webdriver CSharp Bootcamp/Section 3 Variables and Strings/Google Share Code/Section 3 Lesson 5 StringReplacement/StringReplacement/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringReplacement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfSandwiches = 6;
+            int costOfSandwiches = 3;
+
+            Console.WriteLine("The {0} sandwiches cost {1:C} dollars", numberOfSandwiches, costOfSandwiches );
+
+        }
+    }
+}
