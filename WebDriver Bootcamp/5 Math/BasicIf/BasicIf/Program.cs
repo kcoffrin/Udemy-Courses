655e1fda21966cb57e5806acc77aef2733a0ff27
kcoffrin <kcoffrin@gmail.com>
2017-12-27 21:57:34 -0500

    Session 6
    Session 6

---
+++ WebDriver Bootcamp/5 Math/BasicIf/BasicIf/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int aNumber = 5;
+            int aName = "Fred";
+
+            if (aNumber == 10)
+            {
+                //if aNumber is 10 this code will execute
+            }
+            else
+            {
+
+            }
+
+            if (aNumber != 10)
+            {
+                //if aNumber is anything but 10 this code will execute
+            }
+
+            if (aName == "Fred")
+            {
+                //if aName is Fred execute this
+            }
+
+            if (aNumber >= 5)
+            {
+                //If Number is Greater Than or == to 5, execute
+            }
+
+        }
+    }
+}
---
+++ WebDriver Bootcamp/5 Math/DecimalMath/DecimalMath/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecimalMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float aFloatingValue = 0.5F; //USE F to Denote Floating Value
+            double aDoubleValue = 0.5;
+            decimal aDecimalValue = 0.5M; // Use M for Decimal
+            int aIntValue = 5;
+
+            // This is legal: Floating and Double into Double
+            double aDoubleSum = aFloatingValue + aDoubleValue;
+
+            // Double and Float into an INT.  No issue 
+            double aDoubleSum2 = aDoubleValue + aIntValue;
+
+            double aDoubleSum3 = aFloatingValue + aIntValue;
+
+            float aFloatSum = aFloatingValue + aIntValue;
+
+            // Not Legal
+            // Float and a Double and put them into a Float
+            // OK if Double, not Float.  
+            // Outcome could be larger then the Float can hold. 
+
+            float aFloatSum2 = aFloatingValue + aDoubleValue;
+
+            // Decimal and Int can be put into a Decimal Value. OK
+
+            decimal aDecimalSum = aDecimalValue + aIntValue;
+
+            // Not Legal. Float and Double into Decimal. 
+            // Data types do not convert.  Would need to Cast or Convert
+
+            decimal aDecimalSum2 = aFloatingValue + aDoubleValue;
+
+            // Not Legal.  Float to a Decimal.  Different error
+            // Doesnt know what to do with this.  Nothing will work.
+
+            decimal aDecimalSum3 = aFloatingValue + aDecimalValue;
+
+
+        }
+    }
+}
